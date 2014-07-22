4215e7ce629c408a939d64707c2d6cd0bc73e890
ovcharik <maksim.ovcharik@gmail.com>
2014-07-22 22:48:09 +0600

    Params

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 Package required `sass` gem.
 
     $ gem install sass
+    # or check http://sass-lang.com/install
 
 For adding package to project edit you `smart.json` file like as example.
 
@@ -22,11 +23,78 @@ And run `mrt update`.
 
 ## Configuration
 
-Add a `ruby-sass.json` file to root directory of project:
+Add a `ruby-sass.json` file to root directory of project.
 
-### Input
 
-Path to main file 
+**file** `string`
+
+Path to main file.
+
+
+**output** `string`
+
+Path to output file.
+
+
+**unixNewlines** `boolean`, default: `false`
+
+Use Unix-style newlines in written files.
+
+
+**scss** `boolean`, default: `false`
+
+Use the CSS-superset SCSS syntax.
+
+
+**style** `string`, default: `nested`
+
+Output style. Can be `nested`, `compact`, `compressed`, or `expanded`.
+
+
+**precision** `number`, default: `5`
+
+How many digits of precision to use when outputting decimal numbers.
+
+
+**compass** `boolean`, default: `false`
+
+Make Compass imports available and load project configuration.
+
+
+**comments** `boolean`, default: `false`
+
+Emit comments in the generated CSS indicating the corresponding source line.
+
+
+**import** `string`
+
+Add a sass import path.
+
+
+**require** `string`
+
+Require a Ruby library before running Sass.
+
+
+**cacheLocation** `string`
+
+The path to put cached Sass files. Defaults to `.sass-cache`
+
+
+**noCache** `boolean`, default: `false`
+
+Don't cache to sassc files.
+
+
+**sourcemap** `boolean`, default: `false`
+
+Create sourcemap files next to the generated CSS files. `output` required.
+
+
+**defaultEncoding** `string`
+
+Specify the default encoding for Sass files.
+
 
 ### Example
 
--- plugin.js
+++ plugin.js
@@ -2,6 +2,60 @@ var path = Npm.require("path");
 var fs   = Npm.require("fs");
 var sh   = Npm.require('execSync');
 
+
+var optionsBuilder = function(options) {
+  if (typeof options !== "object")
+    return null;
+  if (typeof options.file !== "string")
+    return null;
+
+  var bool = function(name, value) {
+    return function(v) {
+      if (v === value)
+        return name;
+      else
+        return null;
+    }
+  }
+  var param = function(name) {
+    return function(value) {
+      if (value)
+        return name + " " + value;
+      else
+        return null;
+    }
+  }
+  var map = {
+    "style":           param("--style"),
+    "cacheLocation":   param("--cache-location"),
+    "import":          param("--load-path"),
+    "precision":       param("--precision"),
+    "require":         param("--require"),
+    "defaultEncoding": param("--default-encoding"),
+
+    "unixNewlines":    bool("--unix-newlines", true),
+    "comments":        bool("--line-comments", true),
+    "scss":            bool("--scss",          true),
+    "compass":         bool("--compass",       true),
+    "noCache":         bool("--no-cache",      true),
+    "sourcemap":       bool("--sourcemap",     true)
+  }
+
+  var result = [];
+  result.push("sass");
+  result.push(options.file);
+
+  for (var key in map) {
+    var r = map[key](options[key]);
+    if (r) result.push(r);
+  }
+
+  if (options.output) result.push(options.output);
+
+  return result.join(" ");
+}
+
+
 var sourceHandler = function(compileStep) {
   var optionsFile = path.join(process.cwd(), 'ruby-sass.json');
   var options = {};
@@ -10,17 +64,22 @@ var sourceHandler = function(compileStep) {
     var content = fs.readFileSync(optionsFile);
     options = JSON.parse(content);
   }
-  if (options.input) {
-    var r = sh.exec("sass " + options.input);
+  var cmd = optionsBuilder(options);
+  if (cmd) {
+    var r = sh.exec(cmd);
     if (r.code) {
       compileStep.error({
-        message: "Scss compiler error: \n" + r.stderr
+        message: "Sass compiler error: \n" + r.stdout
       });
     }
     else {
+      var data = r.stdout;
+      if (options.output) {
+        data = fs.readFileSync(optionsFile, "utf-8");
+      }
       compileStep.addStylesheet({
-        path: options.input + ".css",
-        data: r.stdout
+        path: options.output || (options.file + ".css"),
+        data: data
       });
     }
   }
