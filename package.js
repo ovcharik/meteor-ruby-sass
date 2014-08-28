db5098ab0a47c058a27da5ff172952269c586983
ovcharik <maksim.ovcharik@gmail.com>
2014-08-29 01:50:34 +0600

    Bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: 'Style with attitude.',
   git: "https://github.com/ovcharik/meteor-ruby-sass.git",
-  version: "0.0.6"
+  version: "0.0.7"
 });
 
 Package._transitional_registerBuildPlugin({
